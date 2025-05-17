da1341f1b1eb32a07306e0158a52a73cc72551f0
Əbil Abilli <ebilebilli3@gmail.com>
2025-05-17 20:16:24 +0400

    Postgresql settings created

--- masters/masters/settings.py
+++ masters/masters/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'masters.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'masteruser',
+        'PASSWORD': 'boceyim123',
+        'HOST': 'localhost',   
+        'PORT': '5432',        
     }
 }
 
@@ -138,3 +142,10 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',  
     ),
 }
+
+#Celery settings
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_IGNORE_RESULT = True
+CELERY_TIMEZONE = 'UTC'
+
