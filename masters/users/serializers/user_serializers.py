dcc9e02b7af301742e6597b918208724da77f4e6
Izzet-Aghayev <agayevizzet56@gmail.com>
2025-06-03 14:32:48 +0400

    change user serializer

--- masters/users/serializers/user_serializers.py
+++ masters/users/serializers/user_serializers.py
@@ -4,11 +4,60 @@ from core.models import City
 from users.models import Language, WorkImage
 
 
+# class RegisterSerializer(serializers.ModelSerializer):
+#     # Əlavə sahələr üçün ID-lər (m2m)
+#     cities = serializers.PrimaryKeyRelatedField(queryset=City.objects.all(), many=True)
+#     languages = serializers.PrimaryKeyRelatedField(queryset=Language.objects.all(), many=True)
+#     work_images = serializers.PrimaryKeyRelatedField(queryset=WorkImage.objects.all(), many=True, required=False)
+
+#     password = serializers.CharField(write_only=True, min_length=8)
+#     password2 = serializers.CharField(write_only=True, min_length=8)
+
+#     class Meta:
+#         model = CustomUser
+#         fields = [
+#             'first_name', 'last_name', 'birth_date', 'mobile_number', 'gender',
+#             'profession_area', 'profession_speciality', 'experience_years',
+#             'cities', 'education', 'education_speciality', 'languages',
+#             'profile_image', 'facebook', 'instagram', 'tiktok', 'linkedin',
+#             'work_images', 'note',
+#             'password', 'password2'
+#         ]
+
+#     def validate(self, attrs):
+#         if attrs.get('password') != attrs.get('password2'):
+#             raise serializers.ValidationError({"password": "Şifrələr uyğun deyil."})
+#         return attrs
+
+#     def create(self, validated_data):
+#         password = validated_data.pop('password')
+#         validated_data.pop('password2')
+
+#         cities = validated_data.pop('cities', [])
+#         languages = validated_data.pop('languages', [])
+#         work_images = validated_data.pop('work_images', [])
+
+#         user = CustomUser.objects.create(**validated_data)
+#         user.set_password(password)
+#         user.save()
+
+#         user.cities.set(cities)
+#         user.languages.set(languages)
+#         user.work_images.set(work_images)
+
+#         return user
+    
+
+
+class WorkImageSerializer(serializers.Serializer):
+    image = serializers.ImageField()
+    order = serializers.IntegerField(required=False)
+
+
 class RegisterSerializer(serializers.ModelSerializer):
-    # Əlavə sahələr üçün ID-lər (m2m)
     cities = serializers.PrimaryKeyRelatedField(queryset=City.objects.all(), many=True)
     languages = serializers.PrimaryKeyRelatedField(queryset=Language.objects.all(), many=True)
-    work_images = serializers.PrimaryKeyRelatedField(queryset=WorkImage.objects.all(), many=True, required=False)
+    work_images = WorkImageSerializer(many=True, required=False)
 
     password = serializers.CharField(write_only=True, min_length=8)
     password2 = serializers.CharField(write_only=True, min_length=8)
@@ -35,7 +84,7 @@ class RegisterSerializer(serializers.ModelSerializer):
 
         cities = validated_data.pop('cities', [])
         languages = validated_data.pop('languages', [])
-        work_images = validated_data.pop('work_images', [])
+        work_images_data = validated_data.pop('work_images', [])
 
         user = CustomUser.objects.create(**validated_data)
         user.set_password(password)
@@ -43,6 +92,11 @@ class RegisterSerializer(serializers.ModelSerializer):
 
         user.cities.set(cities)
         user.languages.set(languages)
-        user.work_images.set(work_images)
+
+        for img_data in work_images_data:
+            image = img_data['image']
+            order = img_data.get('order', 0)
+            work_image = WorkImage.objects.create(image=image, order=order)
+            user.work_images.add(work_image)
 
         return user
