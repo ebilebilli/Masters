e89236c544bc5cbbc5ca964f8e314e4610bc11fd
Əbil Abilli <ebilebilli3@gmail.com>
2025-06-06 19:55:44 +0400

    errors for core import ways fixed

--- masters/core/serializers/education_serializer.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from core.models.education_model import Education
-
-
-class EducationSerializer(serializers.ModelSerializer):  
-    class Meta:
-        model = Education
-        fields = '__all__'
--- masters/core/serializers/language_serializer.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from core.models.language_model import Language
-
-
-class LanguageSerializer(serializers.ModelSerializer):  
-    class Meta:
-        model = Language
-        fields = '__all__'
