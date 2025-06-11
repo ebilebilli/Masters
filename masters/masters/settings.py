33c5d59d305ddef1d1d132c2e61b31bbb5a95f3b
Əbil Abilli <ebilebilli3@gmail.com>
2025-06-11 22:13:21 +0400

    CORS settings created for front

--- masters/masters/settings.py
+++ masters/masters/settings.py
@@ -19,7 +19,8 @@ CSRF_TRUSTED_ORIGINS = ['https://masters-1.onrender.com', 'http://masters-1.onre
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # SECURE_SSL_REDIRECT = True
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS=True 
+CORS_ALLOW_CREDENTIALS=False
     
 
 # Application definition
@@ -49,14 +50,14 @@ INSTALLED_APPS = [
 AUTH_USER_MODEL = 'users.CustomUser'
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware'
 ]
 
 ROOT_URLCONF = 'masters.urls'
