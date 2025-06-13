5f060606e733dc023987834839831a2582dde0d5
Əbil Abilli <ebilebilli3@gmail.com>
2025-06-13 22:51:02 +0400

    Admin py updated for user model

--- masters/users/admin.py
+++ masters/users/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.admin import UserAdmin 
 
 from .models import CustomUser
 from .models.work_image_model import WorkImage
@@ -13,43 +13,34 @@ class WorkImageAdmin(admin.ModelAdmin):
     list_per_page = 20
 
 
-
 @admin.register(CustomUser)
-class CustomUserAdmin(BaseUserAdmin):
+class CustomUserAdmin(UserAdmin):
     model = CustomUser
     list_display = (
-        'first_name', 'last_name', 'mobile_number', 'gender', 'profession_area', 'experience_years', 'is_staff'
+        'first_name', 'last_name', 'mobile_number', 'gender', 'profession_area',
+        'profession_speciality', 'experience_years', 'education', 'is_active',
+        'is_staff', 'created_at'
     )
-    list_filter = ('gender', 'profession_area', 'is_active', 'is_staff')
+    list_filter = ('gender', 'education', 'is_active', 'is_staff')
+    search_fields = ('first_name', 'last_name', 'mobile_number')
+    ordering = ('-created_at',)
+    readonly_fields = ('last_login', 'created_at', 'updated_at')
 
     fieldsets = (
-        ('Personal Information', {
-            'fields': ('first_name', 'last_name', 'birth_date', 'mobile_number', 'gender')
-        }),
-        ('Professional Information', {
-            'fields': ('profession_area', 'profession_speciality', 'custom_profession', 'experience_years', 'cities', 'districts')
-        }),
-        ('Education and Languages', {
-            'fields': ('education', 'education_speciality', 'languages')
-        }),
-        ('Additional Information', {
-            'fields': ('profile_image', 'facebook', 'instagram', 'tiktok', 'linkedin', 'work_images', 'note')
-        }),
-        ('Permissions', {
-            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')
-        }),
-        ('History', {
-            'fields': ('last_login', 'created_at', 'updated_at')
-        }),
+        (None, {'fields': ('mobile_number', 'password')}),
+        ('Şəxsi məlumatlar', {'fields': ('first_name', 'last_name', 'birth_date', 'gender')}),
+        ('Peşə məlumatları', {'fields': ('profession_area', 'profession_speciality', 'experience_years', 'custom_profession', 'cities', 'districts')}),
+        ('Təhsil məlumatları', {'fields': ('education', 'education_speciality', 'languages')}),
+        ('Profil', {'fields': ('profile_image', 'facebook', 'instagram', 'tiktok', 'linkedin', 'work_images', 'note')}),
+        ('İcazələr', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Tarixçə', {'fields': ('last_login', 'created_at', 'updated_at')}),
     )
 
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('mobile_number', 'password', 'is_active', 'is_staff', 'is_superuser')
+            'fields': ('mobile_number', 'first_name', 'last_name', 'birth_date', 'gender', 'password1', 'password2', 'is_active', 'is_staff'),
         }),
     )
 
-    search_fields = ('mobile_number', 'first_name', 'last_name')
-    ordering = ('-created_at',)
-    readonly_fields = ('last_login', 'created_at', 'updated_at')
+    filter_horizontal = ('cities', 'districts', 'languages', 'work_images', 'groups', 'user_permissions')
