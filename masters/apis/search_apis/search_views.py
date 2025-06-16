1abea6262dbcdc2ee3677741cd1eee7e0815f761
Əbil Abilli <ebilebilli3@gmail.com>
2025-06-16 17:54:25 +0400

    Changes for deploy

--- masters/apis/search_apis/search_views.py
+++ masters/apis/search_apis/search_views.py
@@ -1,5 +1,5 @@
 import hashlib
-from rest_framework.views import APIView
+from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.db.models import Q
@@ -9,18 +9,21 @@ from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
 
 from users.models.user_model import CustomUser
+from users.serializers.user_serializers import CustomUserSerializer
 from utils.paginations import CustomPagination
 
 
-class SearchAPIView(APIView):
+
+
+class SearchAPIView(ListAPIView):
     permission_classes = [AllowAny]
+    serializer_class = CustomUserSerializer
     pagination_class = CustomPagination
 
     search_param = openapi.Parameter('search', openapi.IN_QUERY, description="Search query", type=openapi.TYPE_STRING)
     profession_area_id_param = openapi.Parameter('profession_area_id', openapi.IN_QUERY, description="Profession area", type=openapi.TYPE_INTEGER)
     profession_speciality_id_param = openapi.Parameter('profession_speciality_id', openapi.IN_QUERY, description="Profession speciality", type=openapi.TYPE_INTEGER)
     city_id_param = openapi.Parameter('city_id', openapi.IN_QUERY, description="City ID", type=openapi.TYPE_INTEGER)
-    # district_id_param = openapi.Parameter('district_id', openapi.IN_QUERY, description="District ID", type=openapi.TYPE_INTEGER)
     language_id_param = openapi.Parameter('language_id', openapi.IN_QUERY, description="Language ID", type=openapi.TYPE_INTEGER)
     education_param = openapi.Parameter('education', openapi.IN_QUERY, description="Education level", type=openapi.TYPE_STRING)
     experience_years_param = openapi.Parameter('experience_years', openapi.IN_QUERY, description="Experience years", type=openapi.TYPE_INTEGER)
@@ -34,7 +37,6 @@ class SearchAPIView(APIView):
             profession_area_id_param,
             profession_speciality_id_param,
             city_id_param,
-            # district_id_param,
             language_id_param,
             education_param,
             experience_years_param,
@@ -45,18 +47,34 @@ class SearchAPIView(APIView):
         operation_summary="Search and filter masters",
         operation_description="Search with filters and keywords"
     )
-    def get(self, request):
+    def get(self, request, *args, **kwargs):
         query_params = request.GET.urlencode()
         cache_key = f'search_{hashlib.md5(query_params.encode()).hexdigest()}'
         cached_data = cache.get(cache_key)
-        if cached_data:
-            return Response(cached_data, status=status.HTTP_200_OK)
 
+        if cached_data:
+            if isinstance(cached_data, dict):
+                return Response(cached_data, status=status.HTTP_200_OK)
+            else:
+                cache.delete(cache_key)
+
+        queryset = self.get_queryset()
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            paginated_response = self.get_paginated_response(serializer.data)
+            cache.set(cache_key, paginated_response.data, timeout=250)
+            return paginated_response
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def get_queryset(self):
+        request = self.request
         search_query = request.query_params.get('search', '')
         profession_area_id = request.query_params.get('profession_area_id')
         profession_speciality_id = request.query_params.get('profession_speciality_id')
         city_id = request.query_params.get('city_id')
-        # district_id = request.query_params.get('district_id')
         language_id = request.query_params.get('language_id')
         education = request.query_params.get('education')
         experience_years = request.query_params.get('experience_years')
@@ -81,9 +99,6 @@ class SearchAPIView(APIView):
         if city_id:
             queryset = queryset.filter(cities__id=city_id)
 
-        # if district_id:
-        #     queryset = queryset.filter(districts__id=district_id)
-
         if language_id:
             queryset = queryset.filter(languages__id=language_id)
 
@@ -96,27 +111,4 @@ class SearchAPIView(APIView):
             except ValueError:
                 pass
 
-        queryset = queryset.order_by(ordering)
-        paginator = self.pagination_class()
-        paginated_queryset = paginator.paginate_queryset(queryset, request)
-
-        # Sonuçları hazırla
-        response_data = [
-            {
-                'id': master.id,
-                'first_name': master.first_name,
-                'last_name': master.last_name,
-                'profession_area': master.profession_area.name if master.profession_area else '',
-                'profession_speciality': master.profession_speciality.name if master.profession_speciality else '',
-                'city': [city.name for city in master.cities.all()],
-                # 'district': [district.name for district in master.districts.all()],
-                'language': [language.name for language in master.languages.all()],
-                'education': master.education,
-                'experience_years': master.experience_years
-            } for master in paginated_queryset
-        ]
-
-        paginated_response = paginator.get_paginated_response(response_data)
-        cache.set(cache_key,paginated_response.data, timeout=250)
-
-        return Response(paginated_response, status=status.HTTP_200_OK)
+        return queryset.order_by(ordering)
