1402837297d335bade918dee07dbbbd3f482f5ad
Əbil Abilli <ebilebilli3@gmail.com>
2025-06-25 01:23:27 +0400

    Update for Review model,view

--- masters/reviews/admin.py
+++ masters/reviews/admin.py
@@ -19,7 +19,7 @@ class ReviewAdmin(admin.ModelAdmin):
 
     fieldsets = (
         ('Əsas məlumatlar', {
-            'fields': ('user', 'master', 'username', 'rating', 'comment')
+            'fields': ('master', 'username', 'rating', 'comment')
         }),
         ('Dəyərləndirmə kriteriyaları', {
             'fields': (
