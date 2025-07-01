40d31cf1f218bdad27748c2a50188fa57662b763
Əbil Abilli <ebilebilli3@gmail.com>
2025-07-01 16:22:07 +0400

    Changes

--- masters/apis/user_apis/user_views.py
+++ masters/apis/user_apis/user_views.py
@@ -3,9 +3,10 @@ from django.db import transaction
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, serializers
 from rest_framework.exceptions import ValidationError
 from rest_framework.parsers import MultiPartParser, FormParser
+
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.exceptions import TokenError
 from rest_framework_simplejwt.token_blacklist.models import OutstandingToken, BlacklistedToken
