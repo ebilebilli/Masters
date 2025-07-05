920a8069ca85329d81c374e5b6b573bb60c5988a
Əbil Abilli <ebilebilli3@gmail.com>
2025-07-05 17:20:48 +0400

    Changes for cors

--- masters/masters/settings.py
+++ masters/masters/settings.py
@@ -31,28 +31,29 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # CORS settings
 CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",          
-    "http://127.0.0.1:3000",
-    "http://localhost:5173",
-    "http://localhost5173.ecom",     
-    "https://peshekar.online",       
-    "https://www.peshekar.online",    
-    "https://api.peshekar.online", 
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000",          
+#     "http://127.0.0.1:3000",
+#     "http://localhost:5173",
+#     "http://localhost5173.ecom",     
+#     "https://peshekar.online",       
+#     "https://www.peshekar.online",    
+#     "https://api.peshekar.online", 
+# ]
 
 CORS_ALLOW_METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"]
-# CORS_ALLOW_HEADERS = [
-#     "accept",
-#     "accept-encoding",
-#     "authorization",
-#     "content-type",
-#     "dnt",
-#     "origin",
-#     "user-agent",
-#     "x-csrftoken",
-#     "x-requested-with",
-# ]
+
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
 
 CORS_ALLOW_CREDENTIALS = False
 
